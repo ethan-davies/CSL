84142d7d4e1f45f24172192d1b7e2cf789a3dd0c
Ethan Davies <application77863@gmail.com>
2023-10-13 15:57:06 +0100

    Initial Commit

---
+++ src/renderer/index.tsx
@@ -0,0 +1,10 @@
+import './global.css'
+
+import { createRoot } from 'react-dom/client'
+
+import { App } from './pages/App'
+
+const container = document.getElementById('root')
+const root = createRoot(container!)
+
+root.render(<App />)
